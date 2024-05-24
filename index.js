faab3486c336b0989549daac20a3d77b30fa4cfa
Ibrahim Cisse <42039693+ibraheemcisse@users.noreply.github.com>
2024-05-24 19:15:38 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,5 @@
-const functions = require('@google-cloud/functions-framework');
+const { CloudFunctionsFramework } = require('@google-cloud/functions-framework');
 
-functions.http('helloWorld', (req, res) => {
-  res.status(200).send('Hello, World!');
-});
-
-functions.http('createItem', (req, res) => {
-  const item = req.body;
-  res.status(201).json({ message: 'Item created', item });
-});
-
-functions.http('updateItem', (req, res) => {
-  const item = req.body;
-  res.status(200).json({ message: 'Item updated', item });
-});
-
-functions.http('deleteItem', (req, res) => {
-  res.status(200).json({ message: 'Item deleted' });
+CloudFunctionsFramework.http('helloWorld', (req, res) => {
+  res.send('Hello, World!');
 });
